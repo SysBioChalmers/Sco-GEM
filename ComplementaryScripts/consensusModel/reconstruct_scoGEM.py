bd99a97d58dcaa16ebe193a72f21ff0a3203c7f7
Eduard Kerkhoven <eduardk@chalmers.se>
2018-10-17 19:18:01 +0200

    feat: add Sco4 reactions and metabolites
    - after mapping mets and rxns, add most of the Sco4 unique reactions 
    - initially not include: RXN66-447, problematic leucine transporter that forms a loop pumping H+ over the membrane. Should be curated later on

--- ComplementaryScripts/consensusModel/reconstruct_scoGEM.py
+++ ComplementaryScripts/consensusModel/reconstruct_scoGEM.py
@@ -17,10 +17,16 @@ import logging
 import fix_iKS1317_issues
 import fix_sco4_issues
 import add_missing_gene_annotations_sco4
+import add_reactions_from_sco4
 
 SAVE_PATH = "../../ModelFiles/xml/scoGEM.xml"
 iKS1317_PATH = "../../ComplementaryData/models/iKS1317.xml"
+
 SCO4_PATH = "../../ComplementaryData/models/Sco4.xml"
+SCO4_REACTION_MAPPING_FN = "../../ComplementaryData/curation/rxns_iKS1317_vs_Sco4.csv"
+SCO4_METABOLITE_MAPPING_FN =  "../../ComplementaryData/curation/mets_iKS1317_vs_Sco4.csv"
+SCO4_REACTION_ANNOTATION_FN = "../../ComplementaryData/curation/added_sco4_reactions.csv"
+SCO4_METABOLITE_ANNOTATION_FN = "../../ComplementaryData/curation/added_sco4_metabolites.csv"
 
 def reconstruct_scoGEM(model_fn, save_fn = None):
     scoGEM = cobra.io.read_sbml_model(model_fn)
@@ -41,9 +47,13 @@ def reconstruct_scoGEM(model_fn, save_fn = None):
     ## 1c) Add missing / changed gene annotations in iMK1208 identifed in Sco4 / and by Snorre 21.09.2018
     add_missing_gene_annotations_sco4.add_gene_annotations(scoGEM)
 
+    # Part 2: Add reactions from Sco4
+    scoGEM = add_reactions_from_sco4.add_reactions(sco4_model, scoGEM, SCO4_REACTION_MAPPING_FN, SCO4_METABOLITE_MAPPING_FN)
+
     # Save model
     ## Version number
     cobra.io.write_sbml_model(scoGEM, save_fn)
+    cobra.io.save_yaml_model(scoGEM, "../../ModelFiles/yml/scoGEM.yml")
 
 
 if __name__ == '__main__':
