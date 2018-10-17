449a068dda87ad7117e08d4d7a93ed0893af7a06
Eduard Kerkhoven <eduardk@chalmers.se>
2018-10-17 18:57:13 +0200

    fix: scoGEM gene assoc as curated in Sco4
    - some missing gene annotations in iMK1208 were identified in Sco4. These are now applied to scoGEM.

--- ComplementaryScripts/consensusModel/reconstruct_scoGEM.py
+++ ComplementaryScripts/consensusModel/reconstruct_scoGEM.py
@@ -15,9 +15,12 @@ import cobra
 import logging
 
 import fix_iKS1317_issues
+import fix_sco4_issues
+import add_missing_gene_annotations_sco4
 
 SAVE_PATH = "../../ModelFiles/xml/scoGEM.xml"
 iKS1317_PATH = "../../ComplementaryData/models/iKS1317.xml"
+SCO4_PATH = "../../ComplementaryData/models/Sco4.xml"
 
 def reconstruct_scoGEM(model_fn, save_fn = None):
     scoGEM = cobra.io.read_sbml_model(model_fn)
@@ -31,6 +34,12 @@ def reconstruct_scoGEM(model_fn, save_fn = None):
     ## 1a) Issues in iKS1317
     fix_iKS1317_issues.fix(scoGEM)
 
+    ## 1b) Issues in Sco4 v4.00
+    sco4_model = cobra.io.read_sbml_model(SCO4_PATH)
+    fix_sco4_issues.fix(sco4_model)
+
+    ## 1c) Add missing / changed gene annotations in iMK1208 identifed in Sco4 / and by Snorre 21.09.2018
+    add_missing_gene_annotations_sco4.add_gene_annotations(scoGEM)
 
     # Save model
     ## Version number
