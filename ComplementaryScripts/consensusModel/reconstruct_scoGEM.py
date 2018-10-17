a6a2b19716d462e9547e3a916ffa8a39f3b32adb
Eduard Kerkhoven <eduardk@chalmers.se>
2018-10-17 17:30:43 +0200

    feat: initiate reconstruction script

---
+++ ComplementaryScripts/consensusModel/reconstruct_scoGEM.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+This file reconstructs scoGEM, the genome-scale model for Streptomyces coelicolor A3(2).
+Author: Snorre Sulheim
+Created: 27.08.2018
+
+
+# Description
+The scoGEM community model of Streptomyces coelicolor is constructed using the three
+
+"""
+
+
+import cobra
+import logging
+
+SAVE_PATH = "../../ModelFiles/xml/scoGEM.xml"
+iKS1317_PATH = "../../ComplementaryData/models/iKS1317.xml"
+
+def reconstruct_scoGEM(model_fn, save_fn = None):
+    scoGEM = cobra.io.read_sbml_model(model_fn)
+    scoGEM.name = "scoGEM"
+    scoGEM.id = "scoGEM"
+    
+    if save_fn is None:
+        save_fn = model_fn
+
+    # Save model
+    ## Version number
+    cobra.io.write_sbml_model(scoGEM, save_fn)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='reconstruct_scoGEM.log', level=logging.INFO)
+    reconstruct_scoGEM(iKS1317_PATH, SAVE_PATH)
