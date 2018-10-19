9912b7e9009caa087e799fc83e20a9fbee475c29
Eduard Kerkhoven <eduardk@chalmers.se>
2018-10-19 08:33:24 +0200

    feat: initiate prepareGecko script
    - this script will prepare the S. coelicolor specific gecko protein databases

---
+++ ComplementaryScripts/ecModel/prepareGecko.m
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [ecModel,model_data,kcats] = prepareGecko
+%
+% Eduard Kerkhoven, 20118-10-16
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [ecModel,model_data,kcats] = prepareGecko()
+
+end
