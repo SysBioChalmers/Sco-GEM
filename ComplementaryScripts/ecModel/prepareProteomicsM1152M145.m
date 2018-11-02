7d92554b98259989eabbe48eab9db13a2445c173
Eduard Kerkhoven <eduardk@chalmers.se>
2018-11-02 17:01:03 +0100

    feat: normalize proteomics data to 0.4 g/gDCW

--- ComplementaryScripts/ecModel/prepareProteomicsM1152M145.m
+++ ComplementaryScripts/ecModel/prepareProteomicsM1152M145.m
@@ -1,9 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% data = prepareProteomicsM1152M145
+% [data, samples] = prepareProteomicsM1152M145
 %
 % Eduard Kerkhoven, 2018-10-16
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+function [data, samples, genes] = prepareProteomicsM1152M145
 %% Load data
 dat         = readtable('../../ComplementaryData/data/proteomics_M1152M145_2018.csv');
 % Pseudogene SCO7079: only in 5 samples, while no MW provided by KEGG. Just
@@ -17,19 +18,26 @@ data.IDs(1) = [];
 data.genes  = table2array(dat(:,1));
 data.genes  = regexprep(data.genes,'(SCO\d{4}).+','$1'); % Remove trailing characters, not sure why these were included for these genes
 
-
-
 dat         = readtable('../../ComplementaryData/data/proteomics_M1152M145_2018_ID.csv');
 data.sample = strcat(dat.Strain,'_',dat.FermenterID,'_',dat.SamplingID);
 
-load('gecko/databases/ProtDatabaseAllUniprot.mat')
+load('../ecModel/gecko/databases/ProtDatabaseAllUniprot.mat')
 [~, ib]=ismember(data.genes,kegg(:,3));
 data.MW     = cell2mat(kegg(ib,5))./1000; % kDa
 
-%% Normalize for size
+%% Normalize for size, and convert to mmol/g protein
 data.norm   = data.top3 .* data.MW; % correct for each protein's kDa
 data.norm   = data.norm ./ sum(data.norm,1,'omitnan'); % weight ratio (g/g)
-data.norm   = data.norm .* 0.2; % injected protein: 0.2 ug
-data.norm   = data.top3./data.MW;
-%% Total count
-data.size   = sum(data.norm,1,'omitnan');
+data.norm   = data.norm ./ data.MW; % mmol / g protein
+
+%data.norm   = data.norm .* 0.0002; % injected 0.2 ug, new unit: mg/0.2 ug
+%data.norm   = data.norm ./ (data.MW*1000); % convert to mmol/0.2 ug
+
+%% Assume 0.4 g protein / gDCW
+data.norm   = data.norm .* 0.4;
+
+%% Output
+samples = data.sample;
+genes   = data.genes;
+data    = data.norm;
+end
--- ComplementaryScripts/ecModel/simulateProteomics.m
+++ ComplementaryScripts/ecModel/simulateProteomics.m
@@ -4,9 +4,29 @@
 % Eduard Kerkhoven, 2018-10-16
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+load('gecko\models\EcscoGEM.mat')
+
+[data, sample, genes] = prepareProteomicsM1152M145;
+
+f = 0.4
+cd gecko/geckomat/limit_proteins
+
+
+ecModel_pool = constrainPool(ecModel,true(length(ecModel.enzymes),1),0.1*0.4);
 
 
 
-load('gecko\models\scoGEM\EcscoGEM.mat')
+[ecModel_prot,~,~] = constrainEnzymes(ecModel,Ptot,sigma,f);
 
-[f,~] = measureAbundance(ecModel.enzymes);
+% model = constrainEnzymes(model,Ptot,sigma,f,GAM,pIDs,data,gRate,GlucUptake)
+
+[f,~] = measureAbundance(ecModel.enzymes,genes,data(:,1));
+
+gRate       = 0.03427;
+GlucUptake  = 0.51
+ecModel_prot = constrainEnzymes(ecModel,0.4,0.4,f,70,genes,data(:,1),gRate,GlucUptake);
+
+
+
+
+ecModel_batch = modifyKcats(ecModel_batch,obj_Val,modifications,name);
