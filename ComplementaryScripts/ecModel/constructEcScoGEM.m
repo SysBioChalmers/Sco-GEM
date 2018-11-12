9bc3bd34cd902f753b92d5560caa400c26726e4d
Eduard Kerkhoven <eduardk@chalmers.se>
2018-11-12 18:41:34 +0100

    feat: update ecScoGEM reconstruction scripts

--- ComplementaryScripts/ecModel/constructEcScoGEM.m
+++ ComplementaryScripts/ecModel/constructEcScoGEM.m
@@ -10,56 +10,44 @@ format short e
 name        = 'scoGEM';
 org_name    = 'streptomyces coelicolor';
 if nargin < 4
-    version = '0.1';
+    version = datestr(now,'yymmdd'); % E.g. 181109 for 9 November 2018
 end
 
 %Load model
 model       = importModel('../../ModelFiles/xml/scoGEM.xml');
-model                = reorderBiomass(model);
-%Remove blocked rxns + correct model.rev and reorder biomass:
+sol=solveLP(model,1)
+
+%Remove blocked rxns + correct model.rev
 cd gecko/geckomat/change_model
 [model,name,version] = preprocessModel(model,name,version);
-
-
-%Set reaction to irreversible
-model=setParam(model,'lb','BFBP',0); % fructose bisphophatase
 
 %Retrieve kcats & MWs for each rxn in model:
 cd ../get_enzyme_data
 model_data = getEnzymeCodes(model);
 kcats      = matchKcats(model_data,org_name);
-save(['../../models/' name '/data/' name '_enzData.mat'],'model_data','kcats','version')
 
 %Integrate enzymes in the model:
 cd ../change_model
 ecModel                 = readKcatData(model_data,kcats);
 [ecModel,modifications] = manualModifications(ecModel);
-% Modify biomass
 
 ecModel=setParam(ecModel,'eq','EX_glc__D_e',0);
 ecModel=setParam(ecModel,'ub','EX_glc__D_e_REV',0.51);
-ecModel=setParam(ecModel,'obj','BM_growth',1);
 ecModel=setParam(ecModel,'lb','ATPM',0);
 sol=solveLP(ecModel)
 
 save(['../../models/Ec' name '.mat'],'ecModel','modifications') 
 %Constrain model to batch conditions:
-sigma    = 0.4;      %Optimized for glucose
+sigma    = 0.6;      %Optimized for glucose
 Ptot     = 0.4120;    %Assumed constant
 obj_Val=0.03427;
 c_source = 'D-glucose exchange (reversible)'; %Rxn name for the glucose uptake reaction
 cd ../limit_proteins
 
-save(['../../models/Ec' name '.mat'],'ecModel','modifications') 
-[ecModel_batch,OptSigma] = getConstrainedModel(ecModel,c_source,sigma,Ptot,obj_Val,modifications,name);
+[ecModel_pool,OptSigma] = getConstrainedModel(ecModel,c_source,sigma,Ptot,obj_Val,modifications,name);
 disp(['Sigma factor (fitted for growth on glucose): ' num2str(OptSigma)])
 
-%Save output models:
-cd ../../models
-ecModel = saveECmodel(ecModel,toolbox,name,version);
-saveECmodel(ecModel_batch,toolbox,[name '_batch'],version);
-cd ../geckomat
-
+save('gecko/models/ecScoGEM_pool.mat','ecModel_pool')
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- ComplementaryScripts/ecModel/simulateProteomics.m
+++ ComplementaryScripts/ecModel/simulateProteomics.m
@@ -4,15 +4,15 @@
 % Eduard Kerkhoven, 2018-10-16
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-load('gecko\models\EcscoGEM.mat')
+load('gecko\models\ecScoGEM_pool.mat')
 
 [data, sample, genes] = prepareProteomicsM1152M145;
 
-f = 0.4
 cd gecko/geckomat/limit_proteins
+[f,~] = measureAbundance(ecModel.enzymes,genes,data(:,1));
 
 
-ecModel_pool = constrainPool(ecModel,true(length(ecModel.enzymes),1),0.1*0.4);
+ecModel_pool = constrainPool(ecModel,true(length(ecModel.enzymes),1),sigma*f);
 
 
 
@@ -20,7 +20,6 @@ ecModel_pool = constrainPool(ecModel,true(length(ecModel.enzymes),1),0.1*0.4);
 
 % model = constrainEnzymes(model,Ptot,sigma,f,GAM,pIDs,data,gRate,GlucUptake)
 
-[f,~] = measureAbundance(ecModel.enzymes,genes,data(:,1));
 
 gRate       = 0.03427;
 GlucUptake  = 0.51
