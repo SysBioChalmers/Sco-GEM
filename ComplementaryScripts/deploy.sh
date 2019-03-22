91af9be7c6eeb4170caab84c6b2dbf857df821c7
sulheim <ssulheim@gmail.com>
2019-03-22 22:06:34 +0100

    chore: update travis and deploy scripts

--- ComplementaryScripts/deploy.sh
+++ ComplementaryScripts/deploy.sh
@@ -16,22 +16,38 @@ fi
 git config --global user.email "deploy@travis-ci.org"
 git config --global user.name "Travis CI Deployment Bot"
 
-# clone the deploy branch
-cd "${HOME}"
-git clone --quiet --branch=${DEPLOY_BRANCH} https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${DEPLOY_BRANCH} > /dev/null
-
-# copy the results from the current memote run to deploy dir
-cp "${TRAVIS_BUILD_DIR}/results/${TRAVIS_COMMIT}.json" "${HOME}/${DEPLOY_BRANCH}/results/"
-
-# create the report pointing to the history stored in deploy branch
-# need to be in build directory to access git history
-cd "${TRAVIS_BUILD_DIR}"
-memote --directory="${HOME}/${DEPLOY_BRANCH}/results/" --filename="${HOME}/${DEPLOY_BRANCH}/index.html" report
-
-#add, commit and push files
-cd "${HOME}/${DEPLOY_BRANCH}"
-git add .
+git status
+git checkout -- ./ComplementaryScripts/deploy.sh
+memote run --exclusive test_basic #--skip-unchanged
+git status
+git checkout ${DEPLOY_BRANCH}
+ls
+memote --directory="./results/" --filename="index.html"
+git status
+git add "index.html"
 git commit -m "Travis build ${TRAVIS_BUILD_NUMBER}"
-git push --quiet origin "${DEPLOY_BRANCH}" > /dev/null
+git push
+
+
+
+
+# # clone the deploy branch
+# cd "${HOME}"
+# git clone --quiet --branch=${DEPLOY_BRANCH} https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${DEPLOY_BRANCH} > /dev/null
+
+# # copy the results from the current memote run to deploy dir
+# cp "${TRAVIS_BUILD_DIR}/results/${TRAVIS_COMMIT}.json.gz" "${HOME}/${DEPLOY_BRANCH}/results/"
+
+# # create the report pointing to the history stored in deploy branch
+# # need to be in build directory to access git history
+# cd "${TRAVIS_BUILD_DIR}"
+# memote --directory="${HOME}/${DEPLOY_BRANCH}/results/" --filename="${HOME}/${DEPLOY_BRANCH}/index.html" report
+
+# #add, commit and push files
+# cd "${HOME}/${DEPLOY_BRANCH}"
+# git add "results/${TRAVIS_COMMIT}.json.gz"
+# git add "index.html"
+# git commit -m "Travis build ${TRAVIS_BUILD_NUMBER}"
+# git push --quiet origin "${DEPLOY_BRANCH}" > /dev/null
 
 echo "Done."
