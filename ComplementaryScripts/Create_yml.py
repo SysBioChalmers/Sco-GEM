fc363c2961c1c153c0557e3ca72adeaa23cd2436
tjasakumelj <30908699+tjasakumelj@users.noreply.github.com>
2019-05-14 18:19:10 +0200

    Add files via upload

--- ComplementaryScripts/Create_yml.py
+++ ComplementaryScripts/Create_yml.py
@@ -12,6 +12,6 @@ from cobra.io import read_sbml_model
 solver='gurobi';
 scoGEM=cobra.io.read_sbml_model("C:/Users/kumelj/Downloads/OptKnock_050218/scoGEM_model_fix_transporters_140519/scoGEM.xml");
 import yaml
-stream=open("C:/Users/kumelj/Downloads/OptKnock_050218/scoGEM_consensus_community_model/Sco-GEM-fix-transporters_010319/Sco-GEM-fix-transporters/ModelFiles/xml/scoGEM.xml", 'w');
-yaml.dump("C:/Users/kumelj/Downloads/OptKnock_050218/scoGEM_consensus_community_model/Sco-GEM-fix-transporters_010319/Sco-GEM-fix-transporters/ModelFiles/xml/scoGEM.yml", stream);
+stream=open("C:/Users/kumelj/Downloads/OptKnock_050218/scoGEM_consensus_community_model/Sco-GEM-fix-transporters_010319/Sco-GEM-fix-transporters/ModelFiles/scoGEM.yml", 'w');
+#yaml.dump("C:/Users/kumelj/Downloads/OptKnock_050218/scoGEM_consensus_community_model/Sco-GEM-fix-transporters_010319/Sco-GEM-fix-transporters/ModelFiles/scoGEM.yml", stream);
 stream.close()
